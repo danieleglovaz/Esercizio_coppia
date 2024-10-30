75d1769b0764c2290cd9182ae953d3d8a3e2b2f4
danieleglovaz <danielegloazzo@gmail.com>
2024-10-30 15:00:31 +0100

    Manca il plot, questa è la lettura

--- Esercizio4.py
+++ Esercizio4.py
@@ -1,7 +1,14 @@
+import pandas as pd
 
-class Star
-#definizione classe
+class Star:
+    
+    def __init__(self, MsuH, b_y, age_parent):
+       self.MsuH = MsuH
+       self.b_y = b_y
+       self.age_parent = age_parent
   
-#lettura file e assegnazione
+data = pd.read_table('Nemo_6670.dat', sep=' ')
+
+Star = Star(data['MsuH'],data['b-y'],data['age_parent'])
 
 #plottaggio
